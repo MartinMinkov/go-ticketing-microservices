497018c2b0f4ae111bea39d0c9053ea6e6b86cb1
Martin Minkov <minkovlmartin@gmail.com>
2023-07-20 15:02:38 -0700

    refactor(mock.go): replace individual mock user functions with a generic one
    - Replace `MockAuthenticatedUser1`, `MockAuthenticatedUser2`, and `MockAuthenticatedUser3` functions with a single `MockAuthenticatedCookie` function that accepts a JWT string as an argument. This change makes the code more DRY (Don't Repeat Yourself) and flexible, as we can now generate a mock authenticated cookie for any user by simply passing their JWT.
    
    - Add `MockUnauthenticatedCookie` function to generate a cookie for unauthenticated users. This function will be useful for testing scenarios where the user is not authenticated.
    
    - Replace JWT strings in functions with constants `User1JWT`, `User2JWT`, and `User3JWT`. This change improves code readability and maintainability, as we now have a single source of truth for these values.

--- common/pkg/utils/mock.go
+++ common/pkg/utils/mock.go
@@ -19,49 +19,20 @@ Expires=Fri, 21 Jul 2023 09:46:51 GMT
 			"password": "password123"
 	}
 */
-func MockAuthenticatedUser1() *http.Cookie {
+const User1JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpYXQiOjE2ODk4ODk2MTEsImlkIjoiNjRiOWFiNGJjZmVhNmI2MmMxZTIyMjlmIn0.5kE_BCrTi7TwVMAyAN8hL1t_SzMIcbyP6wYJdWd1sXo"
+const User2JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QxQHRlc3QuY29tIiwiaWF0IjoxNjg5ODg5NDQ5LCJpZCI6IjY0YjlhYWE5Y2ZlYTZiNjJjMWUyMjI5ZCJ9.iomMo7sgB-_EOjPAto7UhOTMjGFVO3iry8ND60zD75s"
+const User3JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QyQHRlc3QuY29tIiwiaWF0IjoxNjg5ODg5NzAxLCJpZCI6IjY0YjlhYmE1Y2ZlYTZiNjJjMWUyMjJhMSJ9.IgByMJ6ybTt8nn81WHI2RI6SPvz_ivgbNJvialhKAIQ"
+
+func MockAuthenticatedCookie(jwt string) *http.Cookie {
 	return &http.Cookie{
 		Name:  auth.JWT_COOKIE_NAME,
-		Value: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpYXQiOjE2ODk4ODk2MTEsImlkIjoiNjRiOWFiNGJjZmVhNmI2MmMxZTIyMjlmIn0.5kE_BCrTi7TwVMAyAN8hL1t_SzMIcbyP6wYJdWd1sXo",
+		Value: jwt,
 	}
 }
 
-/*
-Generated from:
-auth-service-jwt
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QxQHRlc3QuY29tIiwiaWF0IjoxNjg5ODg5NDQ5LCJpZCI6IjY0YjlhYWE5Y2ZlYTZiNjJjMWUyMjI5ZCJ9.iomMo7sgB-_EOjPAto7UhOTMjGFVO3iry8ND60zD75s
-Path=/
-HttpOnly
-Expires=Fri, 21 Jul 2023 09:44:09 GMT
-
-	{
-	    "email": "test1@test.com",
-	    "password": "password123"
-	}
-*/
-func MockAuthenticatedUser2() *http.Cookie {
+func MockUnauthenticatedCookie() *http.Cookie {
 	return &http.Cookie{
 		Name:  auth.JWT_COOKIE_NAME,
-		Value: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QxQHRlc3QuY29tIiwiaWF0IjoxNjg5ODg5NDQ5LCJpZCI6IjY0YjlhYWE5Y2ZlYTZiNjJjMWUyMjI5ZCJ9.iomMo7sgB-_EOjPAto7UhOTMjGFVO3iry8ND60zD75s",
-	}
-}
-
-/*
-Generated From:
-auth-service-jwt
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QyQHRlc3QuY29tIiwiaWF0IjoxNjg5ODg5NzAxLCJpZCI6IjY0YjlhYmE1Y2ZlYTZiNjJjMWUyMjJhMSJ9.IgByMJ6ybTt8nn81WHI2RI6SPvz_ivgbNJvialhKAIQ
-Path=/
-HttpOnly
-Expires=Fri, 21 Jul 2023 09:48:21 GMT
-
-	{
-	    "email": "test2@test.com",
-	    "password": "password123"
-	}
-*/
-func MockAuthenticatedUser3() *http.Cookie {
-	return &http.Cookie{
-		Name:  auth.JWT_COOKIE_NAME,
-		Value: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QyQHRlc3QuY29tIiwiaWF0IjoxNjg5ODg5NzAxLCJpZCI6IjY0YjlhYmE1Y2ZlYTZiNjJjMWUyMjJhMSJ9.IgByMJ6ybTt8nn81WHI2RI6SPvz_ivgbNJvialhKAIQ",
+		Value: "",
 	}
 }
