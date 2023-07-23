5593ea59de91bfd4da82f7927826bc3168c138d8
Martin Minkov <minkovlmartin@gmail.com>
2023-07-22 20:48:10 -0700

    feat(ticket_updated_event.go): add NewTicketUpdatedEvent function to create new instances of TicketUpdatedEvent fix(ticket_updated_event.go): change Price type from int to int64 to handle larger price values

--- common/pkg/events/ticket_updated_event.go
+++ common/pkg/events/ticket_updated_event.go
@@ -4,10 +4,22 @@ type TicketUpdatedEventData struct {
 	Id     string `json:"id"`
 	UserId string `json:"user_id"`
 	Title  string `json:"title"`
-	Price  int    `json:"price"`
+	Price  int64  `json:"price"`
 }
 
 type TicketUpdatedEvent struct {
 	Subject Subjects               `json:"subject"`
 	Data    TicketUpdatedEventData `json:"data"`
+}
+
+func NewTicketUpdatedEvent(id string, userId string, title string, price int64) *TicketUpdatedEvent {
+	return &TicketUpdatedEvent{
+		Subject: TicketUpdated,
+		Data: TicketUpdatedEventData{
+			Id:     id,
+			UserId: userId,
+			Title:  title,
+			Price:  price,
+		},
+	}
 }
