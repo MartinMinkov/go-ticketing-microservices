cf67c75ce4fefc4400b966778a1533ded1ff5bde
Martin Minkov <minkovlmartin@gmail.com>
2023-07-30 17:02:11 -0700

    refactor(tests/helpers.go): remove unnecessary goroutine for InitEventListeners for code simplicity feat(tests/helpers.go): add NatsCleanup in app cleanup process to ensure proper cleanup of Nats resources after tests

--- orders/tests/helpers.go
+++ orders/tests/helpers.go
@@ -126,9 +126,7 @@ func SpawnApp() *TestApp {
 	gin.SetMode(gin.ReleaseMode)
 	server := api.BuildServer(config, appState)
 
-	go func() {
-		api.InitEventListeners(appState)
-	}()
+	api.InitEventListeners(appState)
 
 	go func() {
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
@@ -161,6 +159,7 @@ func (app *TestApp) Cleanup() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	defer app.AppState.DBCleanup()
+	defer app.AppState.NatsCleanup()
 
 	if err := app.Server.Shutdown(ctx); err != nil {
 		log.Fatal("Server forced to shutdown: ", err)
