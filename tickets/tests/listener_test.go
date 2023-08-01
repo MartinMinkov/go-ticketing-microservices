f0d9930617465d8af55fe7773bf336c03e794286
Martin Minkov <minkovlmartin@gmail.com>
2023-07-31 19:57:15 -0700

    refactor(listener_test.go): rename TestTicketUpdatedListenerUpdatesTicket to TestOrderUpdatesListenerUnReservesTicket to better reflect test purpose feat(listener_test.go): add test for order creation and cancellation events to ensure ticket reservation and unreservation functionality works as expected

--- tickets/tests/listener_test.go
+++ tickets/tests/listener_test.go
@@ -54,40 +54,60 @@ func TestOrderCreatedListenerReservesTicket(t *testing.T) {
 	}
 }
 
-func TestTicketUpdatedListenerUpdatesTicket(t *testing.T) {
+func TestOrderUpdatesListenerUnReservesTicket(t *testing.T) {
 	app := SpawnApp()
 	defer app.Cleanup()
 
-	// Publish the event with a random ticket id
-	ticket_id := primitive.NewObjectID().Hex()
-	publisher := events.NewPublisher(app.AppState.NatsConn, events.TicketCreated, context.TODO())
-	err := publisher.Publish(events.NewTicketCreatedEvent(ticket_id, primitive.NewObjectID().Hex(), "Test Ticket", 100, 0))
-	if err != nil {
-		log.Err(err).Msg("Failed to publish ticket created event")
+	data := map[string]interface{}{
+		"title": "test ticket",
+		"price": 100,
 	}
 
-	app.Wait(250)
+	user := utils.UserMock1
+	resp := app.PostCreateTicket(data, &user)
+	defer resp.Body.Close()
 
-	publisher1 := events.NewPublisher(app.AppState.NatsConn, events.TicketUpdated, context.TODO())
-	err = publisher1.Publish(events.NewTicketUpdatedEvent(ticket_id, primitive.NewObjectID().Hex(), primitive.NewObjectID().Hex(), "Test Ticket1", 110, 1))
-	if err != nil {
-		log.Err(err).Msg("Failed to publish ticket update event")
+	if resp.StatusCode != http.StatusCreated {
+		t.Errorf("expected status code %d, got %d", http.StatusCreated, resp.StatusCode)
 	}
 
-	app.Wait(250)
+	var ticket model.Ticket
+	if err := utils.ReadJSON(resp, &ticket); err != nil {
+		t.Fatal(err)
+	}
 
-	ticket, err := model.GetSingleTicket(app.AppState.DB, ticket_id)
+	order_id := primitive.NewObjectID().Hex()
+	publisher := events.NewPublisher(app.AppState.NatsConn, events.OrderCreated, context.TODO())
+	err := publisher.Publish(events.NewOrderCreatedEvent(order_id, ticket.UserId, ticket.ID.Hex(), string(events.OrderCreated), 100, time.Now().Add(30*time.Minute)))
+	if err != nil {
+		log.Err(err).Msg("Failed to publish order created event")
+	}
+
+	app.Wait(100)
+	reserved_ticket, err := model.GetSingleTicket(app.AppState.DB, ticket.ID.Hex())
+
 	if err != nil {
 		t.Fatal(err)
 	}
-	if ticket.Title != "Test Ticket1" {
-		t.Errorf("expected title %s, got %s", "Test Ticket", ticket.Title)
+
+	if *reserved_ticket.OrderId != order_id {
+		t.Errorf("expected order id %s, got %s", order_id, *ticket.OrderId)
 	}
-	if ticket.Price != 110 {
-		t.Errorf("expected price %d, got %d", 110, ticket.Price)
+
+	publisher = events.NewPublisher(app.AppState.NatsConn, events.OrderCancelled, context.TODO())
+	err = publisher.Publish(events.NewOrderCancelledEvent(*reserved_ticket.OrderId, ticket.ID.Hex()))
+	if err != nil {
+		log.Err(err).Msg("Failed to publish order cancelled event")
 	}
-	if ticket.Version != 1 {
-		t.Errorf("expected version %d, got %d", 1, ticket.Version)
+
+	app.Wait(100)
+
+	unreserved_ticket, err := model.GetSingleTicket(app.AppState.DB, ticket.ID.Hex())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if unreserved_ticket.OrderId != nil {
+		t.Errorf("expected order id %s, got %s", order_id, *ticket.OrderId)
 	}
 }
 
